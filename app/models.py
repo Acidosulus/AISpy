3a3249226decc408cd6df27795c1134d98ca9a46
Acidosulus <acidos@yandex.ru>
2023-12-08 16:59:45 +0700

    2023-12-08 16:59:45

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timezone
+from hashlib import md5
+from time import time
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import app, db, login
+
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
--- config.py
+++ config.py
@@ -3,14 +3,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 import configparser  # импортируем библиотеку
 import urllib
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     config = configparser.ConfigParser()
     config.read("settings.ini", encoding='UTF-8')  
 
     SECRET_KEY = 'a really really really really long secret key' #os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:321@185.112.225.153:35432/start_dev'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:321@185.112.225.153:35432/start_dev'
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     
     ###   really working connection to STEK
     ## SQLALCHEMY_DATABASE_URI = f'mssql+pymssql://{urllib.parse.quote_plus("КАЗАКОВЦЕВ_НМ")}:{urllib.parse.quote_plus("1")}@{urllib.parse.quote_plus("10.19.50.11:1433")}/{urllib.parse.quote_plus("atom_khk_ul_test.stack")}'
